ce4f9866d22b199a97c783ec0ed43d02f8c4ffa9
Arina-Fathian <arinafathian@gmail.com>
2025-09-02 15:31:24 +0330

    refactor: do some edits on day-08

--- day-08/multiplication-table.js
+++ day-08/multiplication-table.js
@@ -1,12 +1,18 @@
-// simple function
-function multiplicationTable(x, y) {
-  for (let i = 1; i <= x; i++) {
-    for (let j = 1; j <= y; j++) {
-      const multi = x * y;
-      return multi;
-    }
+for (let i = 1; i <= 10; i++) {
+  for (let j = 1; j <= 10; j++) {
+    const multi = i * j;
+    console.log(multi);
   }
 }
 
-const multi = multiplicationTable(2, 6);
-console.log(multi);
+// function multiplicationTable(x, y) {
+//   for (let i = 1; i <= x; i++) {
+//     for (let j = 1; j <= y; j++) {
+//       const multi = x * y;
+//       return multi;
+//     }
+//   }
+// }
+
+// const multi = multiplicationTable(2, 6);
+// console.log(multi);
